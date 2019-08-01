4ab3510df805f9813f0198fc2ee6a2ce4d640258
Simon <48972207+SimonXu0811@users.noreply.github.com>
2019-08-01 21:31:38 +0800

    Create Solution.java

---
+++ 094/Solution.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    List<Integer> list = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        inorder(root);
+        return list;
+    }
+    
+    private void inorder(TreeNode root){
+        if(root == null) return;
+        inorder(root.left);
+        list.add(root.val);
+        inorder(root.right);
+    }
+}
