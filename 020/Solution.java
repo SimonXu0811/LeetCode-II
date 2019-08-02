03d3e8bdcd7753bcafea87d2f52309a2d2368577
Simon <48972207+SimonXu0811@users.noreply.github.com>
2019-08-02 10:19:58 +0800

    Update Solution.java

--- 020/Solution.java
+++ 020/Solution.java
@@ -1,6 +1,7 @@
-public boolean isValid(String s) {
-	Stack<Character> stack = new Stack<Character>();
-	for (char c : s.toCharArray()) {
+class Solution {
+    public boolean isValid(String s) {
+	    Stack<Character> stack = new Stack<Character>();
+	    for (char c : s.toCharArray()) {
 		if (c == '(')
 			stack.push(')');
 		else if (c == '{')
@@ -11,4 +12,5 @@ public boolean isValid(String s) {
 			return false;
 	}
 	return stack.isEmpty();
+}
 }
