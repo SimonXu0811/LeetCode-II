283765f6ed0ded80c1eb8e8e4c08995d35853980
Simon <48972207+SimonXu0811@users.noreply.github.com>
2019-10-03 00:42:35 -0400

    Create Solution.java

---
+++ 225/Solution.java
@@ -0,0 +1,40 @@
+class MyStack {
+    Queue<Integer> que;
+    
+    /** Initialize your data structure here. */
+    public MyStack() {
+        que = new LinkedList<>();
+    }
+    
+    /** Push element x onto stack. */
+    public void push(int x) {
+        que.offer(x);
+        for(int i = 0; i < que.size() - 1; i++){
+            que.offer(que.poll());
+        }
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+         return que.poll();
+    }
+    
+    /** Get the top element. */
+    public int top() {
+        return que.peek();
+    }
+    
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return que.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
